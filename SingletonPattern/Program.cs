45e8d1629b32f62c4388d49e3810bf8f6bbe9d05
Arif Gencosmanoglu <a.gencosmanoglu@gmail.com>
2022-02-17 14:27:47 -0500

    Added singleton design pattern (Thread safe way)

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonPattern;
+
+var singleton1 = Singleton.Instance;
+singleton1.Property = "first val";
+var singleton2 = Singleton.Instance;
+singleton2.Property = "second val";
+
+var singleton3 = Singleton.Instance;
+Console.WriteLine(singleton3.Property);
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        public string Property { get; set; }
+
+        public static readonly Lazy<Singleton> _lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance 
+        { 
+            get
+            {
+                Console.WriteLine("Instance called.");
+                return _lazy.Value;
+            }
+        }
+
+        private Singleton()
+        {
+            // private constructur allows the class not to be instantiated from outside
+            Console.WriteLine("Constuctor invoked.");
+        }
+    }
+}
